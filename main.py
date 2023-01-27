d617fa7b607e0298967776c853a44395831b8768
Ryan <dayry529@gmail.com>
2023-01-27 14:11:59 +1300

    finished

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
 from euchre import euchre
 
 def main():
-    # Match is 5 rounds
-    # round n turns, one each player
-    # Turn one person play a card
-
-    # to do:
-    # bugs: see paper
-    # done: Must follow wont trigger on the left bower
-    # full test
-
-    # Card class has string and num representation but num isnt really used
-    # Even though a num for the suit would make more sense (.lower())
-
     # Number of player, and optional winning score inputs
     euchre(4, 3)
 
